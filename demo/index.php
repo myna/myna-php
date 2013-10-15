9fdc8b8b28b801043b983d916dd78438269614da
Noel Welsh <noel@untyped.com>
2013-10-15 16:56:36 +0100

    Starting on a demo app.
    Simple demo of the client. Untested.

---
+++ demo/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Myna PHP Demo</title>
+</head>
+<body>
+  <h1>Myna PHP Demo</h1>
+<?php
+require_once '../vendor/autoload.php';
+
+$experimentId = '8ebfeab2-c308-413f-93ff-45beb603949a';
+$deploymentUuid = 'ae15f7c0-df1f-11e2-bfc7-7c6d628b25f7';
+$client = \Myna\Myna::init($deploymentUuid);
+$suggestion = $client->suggest($experimentId);
+
+echo "<p>Suggestion is $suggestion</p>";
+?>
+</body>
+</html>
