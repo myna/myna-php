098153115dab21b8257304d0ec9d3d7d313dd577
Noel Welsh <noel@untyped.com>
2013-10-22 12:30:37 +0100

    Enable logging in the demo

--- demo/index.php
+++ demo/index.php
@@ -8,6 +8,9 @@
 <?php
 require_once '../src/autoload.php';
 
+// Turn on logging
+\Myna\Log::$enabled = true;
+
 $experimentId = '8ebfeab2-c308-413f-93ff-45beb603949a';
 $deploymentUuid = 'ae15f7c0-df1f-11e2-bfc7-7c6d628b25f7';
 $client = \Myna\Myna::init($deploymentUuid);
